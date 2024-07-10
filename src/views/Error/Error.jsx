342ff90132e831257eac425c5df5b46e6d20889a
Tên baco0611 <baoblink@gmail.com>
2024-07-10 17:15:54 +0900

    [CREATE] Create error component

--- src/views/Error/Error.jsx
+++ src/views/Error/Error.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import "./Error.scss"
+import Logo from "../../assets/image/Logo.png"
+import { Link } from 'react-router-dom'
 
 export default function Error() {
     return (
-        <div>Error</div>
+        <div className='error'>
+            <img src={Logo}></img>
+            <p>This link seems to not exist. 
+            
+            <br/>Please click <Link to={"/"}>here</Link> to return to the homepage.</p>
+        </div>
     )
 }
